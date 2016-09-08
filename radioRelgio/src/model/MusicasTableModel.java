35d87976b0234bbcbcdd5a6003e0a693adc5696d
a1502727 <a1502727@LAB112M09>
2016-09-08 20:27:59 -0300

    Criação do jTableModel para o jTable.

---
+++ radioRelgio/src/model/MusicasTableModel.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author a1502727
+ */
+public class MusicasTableModel extends AbstractTableModel{
+
+    private ArrayList<Musica> listagem;
+    private String[] colunas = {"Nome", "Tempo"};
+    
+    public MusicasTableModel(){
+        listagem = new ArrayList<>();
+    }
+    
+    public void addRow(Musica m){
+        this.listagem.add(m);
+        this.fireTableDataChanged();
+    }
+    
+    public String getColumnName(int num){
+        return this.colunas[num];
+    }
+    
+    @Override
+    public int getRowCount() {
+        return listagem.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return colunas.length;
+    }
+
+    @Override
+    public Object getValueAt(int linha, int coluna) {
+        return listagem.get(linha).getEnderecoMusica();
+    }
+    
+    public void removeRow(int linha){
+        this.listagem.remove(linha);
+        this.fireTableRowsDeleted(linha, linha);
+    }
+    
+}
